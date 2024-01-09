45276d2bbe89ec9fda8f68626cacf76540ca0a90
Andre Dörr <andre_doerr@web.de>
2024-01-09 17:56:33 +0100

    a

---
+++ pages/03_Configure Hub.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+
+
+#helper functions
+#from helper.BTB_app_helper import get_authenticater_config
+#from helper.BTB_app_helper import make_grid
+
+
+
+st.set_page_config(
+         layout="wide",
+         page_title="Jetvault App",
+         #page_icon="images/BeatTheBookie-Logo.jpg",
+         initial_sidebar_state ="expanded"
+               )
+
+
+#
+# variable declaration
+#
+
+
+
+# build config for authenticator
+# -> not needed here
+
+
+
+# 
+# side bar configuration
+# -> no sidebar
+
+
+
+
+#
+# tab definition
+# -> no tabs
+
+
+
+
+
+#
+# content
+# 
+
