5ff00960124deeeb4daff313f5c0877fc47997f6
Andre Dörr <andre_doerr@web.de>
2024-01-09 17:01:33 +0100

    initial

---
+++ JetvaultApp.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+
+
+#helper functions
+#from helper.BTB_app_helper import get_authenticater_config
+#from helper.BTB_app_helper import make_grid
+
+
+
+st.set_page_config(
+         layout="wide",
+         page_title="Jetvault App",
+         #page_icon="images/BeatTheBookie-Logo.jpg",
+         initial_sidebar_state ="expanded"
+               )
+
+
+#
+# variable declaration
+#
+
+
+
+# build config for authenticator
+
+
+
+
+
+
+# 
+# side bar configuration
+#
+
+
+
+
+
+# tab definition
+
+
+
+
