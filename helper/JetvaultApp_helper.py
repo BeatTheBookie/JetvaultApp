8cc0f284d2b78d8f21b75f36ada9d90e11ef960b
Andre Dörr <andre_doerr@web.de>
2024-01-11 17:31:29 +0100

    a

--- helper/JetvaultApp_helper.py
+++ helper/JetvaultApp_helper.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import snowflake.connector
 
 
 
@@ -12,3 +14,33 @@ def make_grid(cols,rows):
 
 
 
+#
+# Functions to get data from database
+#
+
+
+# get configured stage schema
+def get_stage_config():
+        try:
+            conn = snowflake.connector.connect(
+                user=st.session_state.snowflake_user,
+                password=st.session_state.snowflake_password,
+                account=st.session_state.snowflake_account,
+                #warehouse='your_warehouse',
+                database=st.session_state.snowflake_database,
+                schema=st.session_state.snowflake_schema
+                )            
+
+            # Your SQL query
+            query = "SELECT stage_schema, LOAD_TYPE FROM META.LOAD_CONFIG"
+
+            # Execute the query and fetch results into a DataFrame
+            df = pd.read_sql_query(query, conn)
+
+            # Close the connection
+            conn.close()
+
+            return df
+        except Exception as e:
+            st.error(f"Error executing SQL query: {str(e)}")
+            return None
--- pages/02_Configure Stages.py
+++ pages/02_Configure Stages.py
@@ -5,7 +5,7 @@ import streamlit.components.v1 as components
 
 
 #helper functions
-#from helper.BTB_app_helper import get_authenticater_config
+from helper.JetvaultApp_helper import get_stage_config
 #from helper.BTB_app_helper import make_grid
 
 
@@ -48,3 +48,40 @@ st.set_page_config(
 # content
 # 
 
+st.title("Jetvault Stage Schema Configuration")
+
+
+with st.expander("Information"):
+            
+
+      st.markdown("""
+            <p>
+            
+            For each stage schema, which should be used during the Data Vault configuration.
+            Following loading types can be defined for each schema:
+                  - DELTA:   new and changed records are loaded into the Data Vault tables
+                  - FULL:    additionally adds a delete recognition to the loading process
+                  - HISTORY: Stage tables contain a complate history inclusive the DWH_VALID_FROM column
+            
+            </p>
+            """, unsafe_allow_html=True)
+            
+
+
+
+if "snowflake_account" not in st.session_state or \
+   "snowflake_user" not in st.session_state or \
+   "snowflake_password" not in st.session_state or \
+   "snowflake_database" not in st.session_state or \
+   "snowflake_schema" not in st.session_state:
+    st.error("No connection information. Please save Snowflake connection information first.")
+    st.stop
+
+
+ # Streamlit UI
+st.title("Snowflake Data Editor")
+
+df_stage_config = get_stage_config()
+   
+# Display the result DataFrame using st.dataframe
+st.dataframe(df_stage_config)
