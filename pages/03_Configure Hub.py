08c8fe1c2fedda1bddf0f81daa6ee50127bff549
Andre Dörr <andre_doerr@web.de>
2024-01-15 18:44:52 +0100

    a

--- pages/03_Configure Hub.py
+++ pages/03_Configure Hub.py
@@ -168,7 +168,7 @@ with side_grid[0][0]:
 
                   st.success("Hub Load added successfully!")
 
-with side_grid[1][0]:
+with side_grid[0][1]:
 
       with st.expander("Delete Hub load"):
 
@@ -181,7 +181,7 @@ with side_grid[1][0]:
             """, unsafe_allow_html=True)
 
 
-with side_grid[2][0]:
+with side_grid[0][2]:
 
       with st.expander("Edit Hub load"):
 
