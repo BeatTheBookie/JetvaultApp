f5cd6e43094f843b5c7e7e3632be17d5d82eeb6c
Andre Dörr <andre_doerr@web.de>
2024-01-15 17:48:48 +0100

    a

--- helper/JetvaultApp_helper.py
+++ helper/JetvaultApp_helper.py
@@ -119,6 +119,34 @@ def get_hub_load_config():
         return None
 
 
+# write stage config back to the database
+def push_hub_load_config(df_hub_config):
+    try:
+        conn = snowflake.connector.connect(
+            user=st.session_state.snowflake_user,
+            password=st.session_state.snowflake_password,
+            account=st.session_state.snowflake_account,
+            warehouse = st.session_state.snowflake_warehouse,
+            database=st.session_state.snowflake_database,
+            schema=st.session_state.snowflake_schema
+            )            
+
+        # truncate table
+        conn.cursor().execute("truncate table HUB_LOAD")
+
+        # import data frame in empty table
+        success, nchunks, nrows, _ = write_pandas(conn=conn, 
+                                                df =  df_hub_config,
+                                                table_name = 'HUB_LOAD'
+                                                )
+
+        # Close the connection
+        conn.close()
+        
+    except Exception as e:
+        st.error(f"Error executing SQL query: {str(e)}")
+        return None
+
 
 #
 # Get and save Sat load configuration
--- pages/03_Configure Hub.py
+++ pages/03_Configure Hub.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import pandas as pd
 
 
 
@@ -9,6 +10,7 @@ from helper.JetvaultApp_helper import get_hub_load_config
 from helper.JetvaultApp_helper import get_all_db_schema
 from helper.JetvaultApp_helper import get_tables_by_schema
 from helper.JetvaultApp_helper import get_columns_by_table
+from helper.JetvaultApp_helper import push_hub_load_config
 
 
 st.set_page_config(
@@ -90,36 +92,44 @@ with st.expander("Create new Hub load"):
       # create the whole object for a hub configuration
       #
 
+      #get a list of all available schemas
       df_db_schema = get_all_db_schema()
-     
+      
+      #select box for stage schema
       stage_schema = st.selectbox(
                         label = 'Stage Schema',
                         options = df_db_schema
                         )
       
+      # get all tables inside the stage schema
       df_stage_tables = get_tables_by_schema(stage_schema)
 
-     
+      # select box for filtered tables
       stage_table = st.selectbox(
                         label = 'Stage Table',
                         options = df_stage_tables
                         )
-
+      
+      # hub schema selection based on schema list
       hub_schema = st.selectbox(
                         label = 'Hub Schema',
                         options = df_db_schema
                         )
      
+      # manual input for hub name
       hub_name = st.text_input(
                         label = 'Hub Name'
                         )
 
+      # manual input for hub alias
       hub_alias = st.text_input(
                         label = 'Hub Alias'
                         )
       
+      # get all columns for selected stage table
       df_bk_columns = get_columns_by_table(stage_schema, stage_table)
      
+      # multi selection for business key columns of stage table
       bk_columns = st.multiselect(
                         label = 'Business Key',
                         options = df_bk_columns
@@ -128,6 +138,20 @@ with st.expander("Create new Hub load"):
 
       # Button to save connection info
       if st.button("Save Hub Load"):
+
+            # add entry to data frame
+            new_hub_records = [{'STAGE_SCHEMA' : stage_schema},
+                              {'STAGE_TABLE' : stage_table},
+                              {'HUB_SCHEMA' : hub_schema},
+                              {'HUB_NAME' : hub_name},
+                              {'HUB_ALIAS' : hub_alias},
+                              {'BK_SOURCE_COLUMN_LIST' : bk_columns}]
+            
+            # add new hub load to data frame
+            df_hub_load_config = pd.concat([df_hub_load_config, pd.DataFrame(new_hub_records)], ignore_index=True)
+            
+            push_hub_load_config(df_hub_load_config)
+
             st.success("Hub Load added successfully!")
 
 
